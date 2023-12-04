4bc5cff2803a51d5d61e9ec2c805c87b77b3d741
Ricardo Cannizzaro <ricardo.cannizzaro@gmail.com>
2023-12-04 19:54:05 +0000

    adds script to generate the sorted list of papers from a .bib file

---
+++ scripts/generate_paper_list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+""" A script to generate a sorted list of papers from a .bib BibTeX file
+
+    Author: Ricardo Cannizzaro
+    
+"""
+
+import argparse
+import bibtexparser
+from operator import itemgetter
+
+def load_bib_file(bib_file_path):
+    with open(bib_file_path, 'r', encoding='utf-8') as bib_file:
+        bib_database = bibtexparser.load(bib_file)
+    return bib_database.entries
+
+def sort_by_year(bib_entries):
+    return sorted(bib_entries, key=lambda x: int(x.get('year', 0)), reverse=True)
+
+def print_paper_list(sorted_papers):
+    for idx, paper in enumerate(sorted_papers, start=1):
+        title = paper.get('title', 'No Title')
+        year = paper.get('year', 'No Year')
+        authors = paper.get('author', 'No Authors')
+        conference = paper.get('booktitle', '')
+        journal = paper.get('journal', '')
+        url = paper.get('url', '')
+
+        print(f"{idx}. **{title}** ({year})")
+        print(f"   {authors}. {conference}|{journal}")
+        if url:
+            print(f"   [[Paper]({url})]")
+        print()
+
+def print_paper_list_markdown(sorted_papers):
+    for idx, paper in enumerate(sorted_papers, start=1):
+        title = paper.get('title', 'No Title')
+        year = paper.get('year', 'No Year')
+        authors = paper.get('author', 'No Authors')
+        conference = paper.get('booktitle', '')
+        journal = paper.get('journal', '')
+        url = paper.get('url', '')
+
+        print(f"* **{title}** <br> {authors}. {conference}{journal} {year}.<br>")
+        if url:
+            print(f"  [[Paper]({url})]<br>")
+        else:
+            print("<br>")
+        print("")
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate a sorted list of papers from a BibTeX file.')
+    parser.add_argument('bib_file_path', type=str, help='Path to the BibTeX file')
+
+    args = parser.parse_args()
+
+    bib_entries = load_bib_file(args.bib_file_path)
+    sorted_papers = sort_by_year(bib_entries)
+
+    print("Sorted list of papers:\n")
+    print_paper_list(sorted_papers)
+    print("===============================\n")
+
+    print("\nMarkdown formatted list of papers:\n")
+    print_paper_list_markdown(sorted_papers)
